146dc6067ed18000e03373cec248aef7480052d8
Jesús Urrutia <jurrutia@acid.cl>
2019-02-19 16:29:42 -0300

    added writeFileAsync function to fs module

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,4 +1,5 @@
-import { readFile } from 'fs';
+import { readFile, writeFile } from 'fs';
 import { promisify } from 'util';
 
 export const readFileAsync = promisify(readFile);
+export const writeFileAsync = promisify(writeFile);
