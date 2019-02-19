569ea955d4d078f1b571b4fbc27e15d503b2e02c
Jesús Urrutia <jurrutia@acid.cl>
2019-02-19 15:57:17 -0300

    refactor to use async/await syntax

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import path from 'path';
 
 import { readFileAsync } from './utils/fs';
 import { parse as parseCSV } from './utils/csv';
-import { toStdout } from './utils/output';
 
-const sourcePath = path.join(__dirname, '../data/localidades.csv');
+(async () => {
+  const sourcePath = path.join(__dirname, '../data/localidades.csv');
+  const removeQuotes = field => field.replace(/^["']|["']$/gm, '');
 
-const removeQuotes = field => field.replace(/^["']|["']$/gm, '');
+  const content = parseCSV(String(await readFileAsync(sourcePath)), {
+    extraFieldParsers: [removeQuotes],
+    stripHeaders: true,
+  });
 
-readFileAsync(sourcePath)
-  .then(String)
-  .then(content => parseCSV(content, { extraFieldParsers: [removeQuotes], stripHeaders: true }))
-  .then(toStdout);
+  console.log(content);
+})();
