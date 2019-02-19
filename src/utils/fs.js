01adb241fcc3c32afc1fbbf02faa2e1d097fc9c5
Jesús Urrutia <jurrutia@acid.cl>
2019-02-19 15:55:53 -0300

    use promisify API

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,19 +1,4 @@
-import fs from 'fs';
+import { readFile } from 'fs';
+import { promisify } from 'util';
 
-/**
- * Read file async with Promise API
- *
- * @param {String} filePath
- * @param {String} charset
- */
-export function readFileAsync(filePath, charset = 'utf8') {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, charset, (err, contents) => {
-      if (err) {
-        return reject(err);
-      }
-
-      return resolve(contents);
-    });
-  });
-}
+export const readFileAsync = promisify(readFile);
