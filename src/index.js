2c63c2d242472baae7bfc9a461e5f0b493f740f1
Jesús Urrutia <jurrutia@acid.cl>
2019-02-19 16:01:27 -0300

    change import to parseCSV function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
 import { readFileAsync } from './utils/fs';
-import { parse as parseCSV } from './utils/csv';
+import { parseCSV } from './utils/csv';
 
 (async () => {
   const sourcePath = path.join(__dirname, '../data/localidades.csv');
