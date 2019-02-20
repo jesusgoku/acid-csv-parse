219ab81d98891b27b663a0787964d0e8df915e5f
Jesús Urrutia <jurrutia@acid.cl>
2019-02-20 11:51:14 -0300

    use sort function for sorting module

--- src/sorting.js
+++ src/sorting.js
@@ -1,8 +1,10 @@
+import { sort } from './utils/sorting';
 import communes from '../data/sub-regions-v2.json';
 
-const communesSorted = communes.sort(
-  (a, b) => a.parentId - b.parentId || a.name.localeCompare(b.name),
-);
+const communesSorted = sort(communes, ['parentId', 'name']);
+// const communesSorted = communes.sort(
+//   (a, b) => a.parentId - b.parentId || a.name.localeCompare(b.name),
+// );
 
 // communesSorted.forEach(commune =>
 //   console.log(`${commune.parentId}.- ${commune.name}`),
