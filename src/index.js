4f0b0653a2464ed6c0e0d5dadbdc476b74df3235
Cristian Azócar <cazocar@acid.cl>
2019-11-25 15:14:17 -0300

    Change field names to match Peru CSV

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import { readFileAsync } from './utils/fs';
 import { parseCSV } from './utils/csv';
 
 (async () => {
-  const sourcePath = path.join(__dirname, '../data/localidades.csv');
+  const sourcePath = path.join(__dirname, '../data/comunas.csv');
 
   const removeQuotes = field => field.replace(/^["']|["']$/gm, '');
-  const lineToSubRegion = ([commerceId, commerceParentId, name]) => ({
+  const lineToSubRegion = ([commerceId, commerceParentId, parentId, active, id, name, field1, optCounter, xCiudadId, lat, lng]) => ({
+    id,
+    name,
+    lat: Number(lat),
+    lng: Number(lng),
+    parentId,
     commerceId,
     commerceParentId,
-    name,
   });
 
   const content = parseCSV(String(await readFileAsync(sourcePath)), {
